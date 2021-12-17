a08bc8e11926d41034d133515545991958724246
samlerman <slerman@ur.rochester.edu>
2021-12-16 21:53:58 -0500

    should be DrQV2 exactly

--- Agents/DPG.py
+++ Agents/DPG.py
@@ -37,11 +37,12 @@ class DPGAgent(torch.nn.Module):
         self.encoder = CNNEncoder(obs_shape, optim_lr=lr).to(device)
 
         self.critic = EnsembleQCritic(self.encoder.repr_shape, feature_dim, hidden_dim, action_shape[-1],
+                                      l2_norm=False,
                                       ensemble_size=2,
                                       target_tau=target_tau, optim_lr=lr).to(device)
 
         self.actor = TruncatedGaussianActor(self.encoder.repr_shape, feature_dim, hidden_dim, action_shape[-1],
-                                            l2_norm=True, discrete=discrete,
+                                            l2_norm=False, discrete=discrete,
                                             stddev_schedule=stddev_schedule, stddev_clip=stddev_clip,
                                             optim_lr=lr).to(device)
         # Data augmentation
