c2f90dd8f3955900acfaf0b0e6adf2a3e8d8ba9c
samlerman <slerman@ur.rochester.edu>
2021-12-16 21:03:13 -0500

    debugging

--- Blocks/Critics.py
+++ Blocks/Critics.py
@@ -94,7 +94,7 @@ class EnsembleQCritic(nn.Module):
 
         # Dist
         stddev, mean = torch.std_mean(Qs, dim=0)
-        print((stddev <= 0).any(), "huhh?")
+        print((stddev < 0).any(), "huh?")
         Q = Normal(mean, stddev)
         Q.__dict__.update({'Qs': Qs,
                            'action': action})
