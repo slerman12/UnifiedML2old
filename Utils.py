3d3e47418d28d6c3ba21461f44d1097909c2777c
samlerman <slerman@ur.rochester.edu>
2021-12-17 15:23:34 -0500

    debug

--- Utils.py
+++ Utils.py
@@ -123,11 +123,13 @@ def rclamp(x, min, max):
 
 
 # (Multi-dim) indexing
-def gather_index(item, ind):
+def gather_index(item, ind, dim=-1):
     ind = ind.long().expand(*item.shape[:dim], ind.shape[-1])
-    # ind = ind.view(*ind.shape, *[1 for _ in range(len(item.shape[dim:]) - 1)])
+    ind = ind.view(*ind.shape, *[1 for _ in range(len(item.shape[dim:]) - 1)])
 
-    return torch.gather(item, -1, ind)
+    print(item.shape, torch.gather(item, dim, ind).shape)
+
+    return torch.gather(item, dim, ind)
 
 
 # Basic L2 normalization
