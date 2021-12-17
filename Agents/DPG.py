2594aa0584b97c61d95d2b5593e630a73252b2ea
samlerman <slerman@ur.rochester.edu>
2021-12-17 16:11:24 -0500

    debugging gather indices

--- Agents/DPG.py
+++ Agents/DPG.py
@@ -82,7 +82,7 @@ class DPGAgent(torch.nn.Module):
                     action = action.uniform_(-1, 1)
 
             if self.discrete:
-                action = torch.argmax(action, -1).squeeze()
+                action = torch.argmax(action, -1).squeeze(-1)
 
             return action
 
