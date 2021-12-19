5b4ccb08c00ea72580798b328ac1faaa136f8932
samlerman <slerman@ur.rochester.edu>
2021-12-19 18:03:59 -0500

    debugging/testing

--- RunParallel.py
+++ RunParallel.py
@@ -75,6 +75,7 @@ def reinforce(args, root_path):
     converged = False
     while True:
         def evaluate_and_rollout():
+            print("okay...")
             # Evaluate
             if agent.episode % args.evaluate_per_episodes == 0:
 
