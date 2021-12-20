75d51fd86324961ca0c9086de9ff02c8a38c6ad3
samlerman <slerman@ur.rochester.edu>
2021-12-20 11:56:35 -0500

    testing extended env

--- Datasets/Experiences/ExperienceReplay.py
+++ Datasets/Experiences/ExperienceReplay.py
@@ -105,7 +105,10 @@ class ExperienceReplay:
             try:
                 self.episode[spec['name']] = np.array(self.episode[spec['name']], spec['dtype'])
             except TypeError:
-                self.episode[spec['name']] = np.array(self.episode[spec['name']])  # Handling Nones
+                # Handling Nones
+                alias_episode = [np.nan if val is None else val for val in self.episode[spec['name']]]
+                self.episode[spec['name']] = np.array(alias_episode, spec['dtype'])
+                # alias_episode[alias_episode == -999425] = np.nan
 
         timestamp = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
         episode_name = f'{timestamp}_{self.num_episodes}_{self.episode_len}.npz'
