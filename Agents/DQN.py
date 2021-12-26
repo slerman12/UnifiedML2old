ace8d894ffee2ce62bf8518618d0c4e2ff671157
samlerman <slerman@ur.rochester.edu>
2021-12-26 13:35:25 -0500

    editing agents

--- Agents/DQN.py
+++ Agents/DQN.py
@@ -68,7 +68,7 @@ class DQNAgent(torch.nn.Module):
             # "Candidate actions"
             creations = None if self.discrete \
                 else self.creator(obs).sample(self.num_actions)
-            print(self.discrete)
+            print(creations.shape)
 
             # DQN actor is based on critic
             Pi = self.actor(self.critic(obs, creations), self.step)
