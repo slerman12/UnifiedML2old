cf027da3af1741718886d82cbf809d192ce8140c
samlerman <slerman@ur.rochester.edu>
2021-12-27 15:35:00 -0500

    debug

--- Hyperparams/task/classify/generate_classify.py
+++ Hyperparams/task/classify/generate_classify.py
@@ -21,6 +21,7 @@ for task in IMAGE_DATASETS:
 suite: classify
 frame_stack: null
 action_repeat: null
+seed_steps: 10
 task_name: {}""".format(task))
     f.close()
     out += ' "' + task.lower() + '"'
