dcd5abf710d634bc224f89d9977b80242b97e308
samlerman <slerman@ur.rochester.edu>
2021-12-27 17:05:08 -0500

    debug

--- Agents/DQN.py
+++ Agents/DQN.py
@@ -121,7 +121,7 @@ class DQNAgent(torch.nn.Module):
             # Infer
             action = self.actor(self.critic(obs[instruction], creations), self.step).best
 
-            mistake = cross_entropy(action, label[instruction], reduction='none')
+            mistake = cross_entropy(action, label[instruction].long(), reduction='none')
 
             # Supervised loss
             supervised_loss = mistake.mean()
