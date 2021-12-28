703698c66606d3aa409d3e9467ae26f31fac43a9
samlerman <slerman@ur.rochester.edu>
2021-12-28 13:21:09 -0500

    debug

--- Datasets/ExperienceReplay.py
+++ Datasets/ExperienceReplay.py
@@ -120,6 +120,7 @@ class ExperienceReplay:
                 print(max_length, exp[spec['name']].shape, len(exp[spec['name']]))
                 ratio = max_length / exp[spec['name']].shape[0]
                 exp[spec['name']] = np.repeat(exp[spec['name']], ratio, axis=0)
+                print(max_length, exp[spec['name']].shape, len(exp[spec['name']]))
 
                 # Make sure everything is formatted and consistent
                 assert spec['shape'][0] == max_length, 'Batch sizes could not be broadcast for ' + spec['name']
