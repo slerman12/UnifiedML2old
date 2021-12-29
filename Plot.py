b96c6305410867daacfa8e705569a41656732d49
samlerman <slerman@ur.rochester.edu>
2021-12-28 19:55:11 -0500

    plotting/logging

--- Plot.py
+++ Plot.py
@@ -112,7 +112,7 @@ def plot(path='./', experiments=None, suites=None, tasks=None, agents=None):
         if 'classify' in task.lower():
             ax.set_ybound(0, 1)
             ax.yaxis.set_major_formatter(FuncFormatter('{:.0%}'.format))
-            ax.set_ylabel('EvalUation Accuracy')
+            ax.set_ylabel('Test Accuracy')
 
 
     plt.tight_layout()
