2b5781821249b04079d7e90773f08ef126bf7b86
samlerman <slerman@ur.rochester.edu>
2021-12-29 10:29:51 -0500

    plot suites

--- Plot.py
+++ Plot.py
@@ -18,7 +18,7 @@ from matplotlib.ticker import FuncFormatter
 import seaborn as sns
 
 
-def plot(path='./', experiments=None, suites=None, tasks=None, agents=None):
+def plot(path, experiments=None, suites=None, tasks=None, agents=None):
 
     path_dirs = path.split('/')
     path_dir = Path('/'.join(path_dirs[:-1]).replace('Agents.', ''))
@@ -94,7 +94,7 @@ def plot(path='./', experiments=None, suites=None, tasks=None, agents=None):
 
     fig, axs = plt.subplots(num_rows, num_cols, figsize=(4 * num_cols, 3 * num_rows))
 
-    # Plot
+    # Plot tasks
     for i, task in enumerate(suite_tasks):
         data = df[df['Task'] == task]
         task = ' '.join([task_name.capitalize() for task_name in task.split('_')])
@@ -117,6 +117,101 @@ def plot(path='./', experiments=None, suites=None, tasks=None, agents=None):
 
     plt.tight_layout()
     plt.savefig(path)
+
+    # Atari
+    random = {
+        'Alien': 227.8,
+        'Amidar': 5.8,
+        'Assault': 222.4,
+        'Asterix': 210.0,
+        'BankHeist': 14.2,
+        'BattleZone': 2360.0,
+        'Boxing': 0.1,
+        'Breakout': 1.7,
+        'ChopperCommand': 811.0,
+        'CrazyClimber': 10780.5,
+        'DemonAttack': 152.1,
+        'Freeway': 0.0,
+        'Frostbite': 65.2,
+        'Gopher': 257.6,
+        'Hero': 1027.0,
+        'Jamesbond': 29.0,
+        'Kangaroo': 52.0,
+        'Krull': 1598.0,
+        'KungFuMaster': 258.5,
+        'MsPacman': 307.3,
+        'Pong': -20.7,
+        'PrivateEye': 24.9,
+        'Qbert': 163.9,
+        'RoadRunner': 11.5,
+        'Seaquest': 68.4,
+        'UpNDown': 533.4
+    }
+    human = {
+        'Alien': 7127.7,
+        'Amidar': 1719.5,
+        'Assault': 742.0,
+        'Asterix': 8503.3,
+        'BankHeist': 753.1,
+        'BattleZone': 37187.5,
+        'Boxing': 12.1,
+        'Breakout': 30.5,
+        'ChopperCommand': 7387.8,
+        'CrazyClimber': 35829.4,
+        'DemonAttack': 1971.0,
+        'Freeway': 29.6,
+        'Frostbite': 4334.7,
+        'Gopher': 2412.5,
+        'Hero': 30826.4,
+        'Jamesbond': 302.8,
+        'Kangaroo': 3035.0,
+        'Krull': 2665.5,
+        'KungFuMaster': 22736.3,
+        'MsPacman': 6951.6,
+        'Pong': 14.6,
+        'PrivateEye': 69571.3,
+        'Qbert': 13455.0,
+        'RoadRunner': 7845.0,
+        'Seaquest': 42054.7,
+        'UpNDown': 11693.2
+    }
+
+    fig, axs = plt.subplots(1, 3, figsize=(12, 3))
+
+    # Plot suites
+    for col, suite in enumerate(['atari', 'dmc', 'classify']):
+        data = df[suite in df['Task'].lower()]
+        if data.empty:
+            continue
+        data.columns = [' '.join([name.capitalize() for name in col_name.split('_')]) for col_name in data.columns]
+
+        # Human-normalize Atari
+        if suite == 'atari':
+            for task in data.Task.unique():
+                for game in random:
+                    if game.lower() in task.lower():
+                        data[data['Task'] == task] = (data[data['Task'] == task] - random[game]) \
+                                                     / (human[game] - random[game])
+
+        ax = axs[col]
+        hue_order = np.sort(data.Agent.unique())
+
+        sns.lineplot(x='Step', y='Reward', data=data, ci='sd', hue='Agent', hue_order=hue_order, ax=ax)
+        ax.set_title(f'{suite}')
+
+        if suite == 'classify':
+            ax.set_ybound(0, 1)
+            ax.yaxis.set_major_formatter(FuncFormatter('{:.0%}'.format))
+            ax.set_ylabel('Eval Accuracy')
+        elif suite == 'atari':
+            ax.set_ybound(0, 1)
+            ax.set_ylabel('Normalized Reward')
+        elif suite == 'dmc':
+            ax.set_ybound(0, 1000)
+
+    plt.tight_layout()
+    plt.savefig(path_dir / 'Suites.png')
+
     plt.close()
 
 
