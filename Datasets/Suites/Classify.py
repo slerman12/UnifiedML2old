cf2b998e3ab6468463e91cd82a129a980517c995
samlerman <slerman@ur.rochester.edu>
2021-12-29 12:58:43 -0500

    debugged...

--- Datasets/Suites/Classify.py
+++ Datasets/Suites/Classify.py
@@ -59,7 +59,8 @@ class ClassifyEnv:
 
     @property
     def depleted(self):
-        is_depleted = self.count > self.length and self.enable_depletion
+        # '+1 due to the call to self.batch in observation_spec
+        is_depleted = self.count > self.length + 1 and self.enable_depletion
 
         if self.verbose:
             if is_depleted:
@@ -97,7 +98,6 @@ class ClassifyEnv:
     def observation_spec(self):
         if not hasattr(self, 'observation'):
             self.observation = np.array(self.batch[0])
-            self.count = 0
         return specs.BoundedArray(self.observation.shape[1:], self.observation.dtype, 0, 255, 'observation')
 
     def action_spec(self):
