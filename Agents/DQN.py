f9968e71f0db02af65b64bd67c950af9f30c3f0c
samlerman <slerman@ur.rochester.edu>
2021-12-29 18:45:20 -0500

    cheetah run works, mnist works

--- Agents/DQN.py
+++ Agents/DQN.py
@@ -44,8 +44,10 @@ class DQNAgent(torch.nn.Module):
 
         # Continuous actions creator
         self.creator = None if self.discrete \
-            else GaussianActorEnsemble(self.encoder.repr_shape, feature_dim, hidden_dim, self.action_dim, num_actors,
-                                       stddev_schedule=stddev_schedule, stddev_clip=stddev_clip, optim_lr=lr)
+            else GaussianActorEnsemble(self.encoder.repr_shape, feature_dim, hidden_dim,
+                                       self.action_dim, ensemble_size=num_actors,
+                                       stddev_schedule=stddev_schedule, stddev_clip=stddev_clip,
+                                       optim_lr=lr)
 
         self.critic = EnsembleQCritic(self.encoder.repr_shape, feature_dim, hidden_dim, self.action_dim,
                                       discrete=discrete, optim_lr=lr, target_tau=target_tau)
