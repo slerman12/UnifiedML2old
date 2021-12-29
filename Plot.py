6ffc59a696cbf7b04b151111c83d123282ef7ffd
samlerman <slerman@ur.rochester.edu>
2021-12-28 19:27:40 -0500

    plotting/logging

--- Plot.py
+++ Plot.py
@@ -108,7 +108,7 @@ def plot(path='./', experiments=None, suites=None, tasks=None, agents=None):
 
         if 'classify' in task.lower():
             y_axis = 'Eval Accuracy (%)'
-            data[y_axis] = data['Reward'].copy()
+            data.loc[:, y_axis] = data['Reward']
             ax.yaxis.set_major_formatter(FuncFormatter('{0:.0%}'.format))
         else:
             y_axis = 'Reward'
