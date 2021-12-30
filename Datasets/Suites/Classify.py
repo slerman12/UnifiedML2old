d7f1063daea2b7f3587a35067f8b5b3aa9d3cd1b
samlerman <slerman@ur.rochester.edu>
2021-12-29 19:14:38 -0500

    trying custom transform now

--- Datasets/Suites/Classify.py
+++ Datasets/Suites/Classify.py
@@ -131,8 +131,8 @@ def make(task, frame_stack=4, action_repeat=4, max_episode_frames=None, truncate
         def __call__(self, sample):
             sample = F.to_tensor(sample)
             sample *= 255  # Encoder expects pixels
-            mean = stddev = [0.5] * sample.shape[0]  # Depending on num channels
-            sample = F.normalize(sample, mean, stddev)  # Generic normalization
+            # mean = stddev = [0.5] * sample.shape[0]  # Depending on num channels
+            # sample = F.normalize(sample, mean, stddev)  # Generic normalization
             return sample
 
     transform = Transform()
