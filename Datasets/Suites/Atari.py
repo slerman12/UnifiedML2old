d9d8be0b3cc574c40b14aac03c80740cda8da087
samlerman <slerman@ur.rochester.edu>
2022-01-05 20:32:55 -0500

    env debug game mode

--- Datasets/Suites/Atari.py
+++ Datasets/Suites/Atari.py
@@ -220,7 +220,7 @@ def make(task, frame_stack=4, action_repeat=4, max_episode_frames=None, truncate
     # full_action_set = env.getLegalActionSet()
     # env = gym.make(task, full_action_space=False)  # For minimal action spaces
     env.seed(seed)
-    env = AtariPreprocessing(env, frame_skip=action_repeat if train else action_repeat,  # Recommended: 4
+    env = AtariPreprocessing(env, frame_skip=action_repeat if train else action_repeat,  # Recommended: 4  TODO eval 1
                              terminal_on_life_loss=not recommended, screen_size=84)
 
     # Stack several frames
