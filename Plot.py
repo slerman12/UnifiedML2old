c4c9083aa0cc45ec67f84db688a4d62b603c053c
samlerman <slerman@ur.rochester.edu>
2022-01-05 20:35:43 -0500

    plot debug

--- Plot.py
+++ Plot.py
@@ -95,7 +95,7 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
 
         # Rolling max per run
         max_csv = csv.copy()
-        max_csv['Reward'] = max_csv[['reward', 'step']].rolling(length, min_periods=1, on='step').max()['reward']
+        max_csv['reward'] = max_csv[['reward', 'step']].rolling(length, min_periods=1, on='step').max()['reward']
 
         csv_list.append(csv)
         max_csv_list.append(max_csv)
